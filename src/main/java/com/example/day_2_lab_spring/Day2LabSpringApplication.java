f6644c4aee756d604ab8af8ea28a27c2a20377a1
Umar Twynam <umartwynam@gmail.com>
2023-04-18 15:44:38 +0100

    initial upload

---
+++ src/main/java/com/example/day_2_lab_spring/Day2LabSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.day_2_lab_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day2LabSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day2LabSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/day_2_lab_spring/Day2LabSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.day_2_lab_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day2LabSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
